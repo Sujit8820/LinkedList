6f1f35f555e33e121f19833db7fec05fef226bff
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-26 19:23:57 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace LinkedList
+{
+    class program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Linked List");
+
+        }
+    }
+}

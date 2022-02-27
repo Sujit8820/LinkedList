d09dab69f36e17493cc9d5178c14bd5b0d915d0e
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-27 16:17:57 +0530

    [Sujit] ADDED: Ordered linked list in ascending order

--- Program.cs
+++ Program.cs
@@ -10,20 +10,21 @@ namespace LinkedList
         {
             Console.WriteLine("Wellcome to Linked List");
             Operation operation = new Operation();
+            SortedLinkedList sort = new SortedLinkedList();
 
             Node node1 = new Node(56);
             Node node2 = new Node(30);
             Node node3 = new Node(40);
             Node node4 = new Node(70);
-            operation.AddingFromEnd(node1);
-            operation.AddingFromEnd(node2);
-            operation.AddingFromEnd(node3);
-            operation.AddingFromEnd(node4);
-            operation.Dispiay();
+           
+            sort.Sort(node1);
+            sort.Sort(node2);
+            sort.Sort(node3);
+            sort.Sort(node4);
+            
+            sort.Dispiay();
             Console.WriteLine("==============");
-            operation.DeleteElement(40);
-            operation.Dispiay();
-
+            
 
         }
     }
---
+++ SortedLinkedList.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class SortedLinkedList
+    {
+        Node head = null;
+
+        public void Sort(Node data)
+        {
+            if (head == null)
+            {
+                head = data;
+            }
+            else
+            {
+                Node temp = data;
+                int compare = temp.data.CompareTo(head.data);
+                switch (compare)
+                {
+                    case 0:
+                        temp.next = head;
+                        head = temp;
+                        break;
+                    case -1:
+                        temp.next = head;
+                        head = temp;
+                        break;
+                    default:
+                        {
+                            Node p =head;
+                            while(p.next.data < temp.data)
+                            {
+                                p = p.next;
+                                if(p.next == null)
+                                {
+                                    break;
+                                    p.next = temp;
+                                }
+                            }
+                            temp.next = p.next;
+                            p.next = temp;
+                            break;
+                        }
+                }
+
+            }
+
+
+        }
+        public void Dispiay()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("List is empty");
+            }
+            else if (head != null)
+            {
+                int count = 0;
+                Node p = head;
+                while (p != null)
+                {
+                    Console.WriteLine(p.data);
+                    count++;
+                    p = p.next;
+                }
+                Console.WriteLine($"\nSize of list is {count}");
+            }
+        }
+    }
+}
